845135973d40f042dbe421985d2fed5299bc05ef
François Caillet <Francois.caillet@calopea.com>
2016-01-25 09:09:11 +0100

    Init project Maven, Init project with hello world

---
+++ src/main/java/Helloworld.java
@@ -0,0 +1,19 @@
+import org.eclipse.californium.core.CoapServer;
+
+/**
+ * Created by François Caillet on 25/01/2016.
+ * All rights reserved.
+ */
+
+
+class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // binds on UDP port 5683
+        CoapServer server = new CoapServer();
+
+        // TODO :)
+    }
+
+}
